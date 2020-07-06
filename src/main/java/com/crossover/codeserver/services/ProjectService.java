ebe79c37f6d40d8e9133879e35c8b0f33920f186
Dumpa Mounica Reddy <Dumpa_Mounica_Reddy@gap.com>
2020-07-06 21:39:04 +0530

    Made changes for post and pull request

--- src/main/java/com/crossover/codeserver/services/ProjectService.java
+++ src/main/java/com/crossover/codeserver/services/ProjectService.java
@@ -1,8 +1,11 @@
 package com.crossover.codeserver.services;
 
+import com.crossover.codeserver.dto.ProjectDto;
 import com.crossover.codeserver.entities.Project;
 
 public interface ProjectService {
 
     Project getProject(long id);
+    Project createProject(Long SystemId,ProjectDto projectDto);
+    Project updateProject(Long projectId,ProjectDto projectDetails);
 }
--- src/main/java/com/crossover/codeserver/services/ProjectServiceImpl.java
+++ src/main/java/com/crossover/codeserver/services/ProjectServiceImpl.java
@@ -1,9 +1,15 @@
 package com.crossover.codeserver.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.crossover.codeserver.dto.ProjectDto;
 import com.crossover.codeserver.entities.Project;
+import com.crossover.codeserver.entities.SdlcSystem;
 import com.crossover.codeserver.repositories.ProjectRepository;
+import com.crossover.codeserver.repositories.SdlcSystemRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +18,38 @@ import lombok.RequiredArgsConstructor;
 public class ProjectServiceImpl implements ProjectService {
 
 	private final ProjectRepository projectRepository;
+	
+	@Autowired
+	private SdlcSystemRepository sdlcSystemRepository;
 
 	public Project getProject(long id) {
 		return projectRepository.findById(id).get();
 	}
+	
+	public Project createProject( Long systemId,ProjectDto projectDto) {
+	
+		Project project = Project.builder().externalId(projectDto.getExternalId()).name(projectDto.getName())
+				.createdDate(projectDto.getCreatedDate()).lastModifiedDate(projectDto.getLastModifiedDate())
+				.sdlcSystem(buildSdlcSystem(projectDto)).build();
+		sdlcSystemRepository.findById(systemId).ifPresent(project::setSdlcSystem);
+        return projectRepository.save(project);
+	}
+	
+	public Project updateProject(Long projectId,ProjectDto projectDetails) {
+		return projectRepository.findById(projectId).map(project -> {
+                    project.setExternalId(projectDetails.getExternalId());
+                    project.setName(projectDetails.getName());
+                    project.setSdlcSystem(buildSdlcSystem(projectDetails));
+                    final Project updatedProject = projectRepository.save(project);
+                    return updatedProject;
+                }).orElse(null);
+        
+    }
+	private SdlcSystem buildSdlcSystem(ProjectDto projectDetails) {
+	 return SdlcSystem.builder().baseUrl(projectDetails.getSdlcSystem().getBaseUrl())
+			.description(projectDetails.getSdlcSystem().getDescription())
+			.createdDate(projectDetails.getSdlcSystem().getCreatedDate())
+			.lastModifiedDate(projectDetails.getSdlcSystem().getLastModifiedDate()).build();
+}
+	
 }
